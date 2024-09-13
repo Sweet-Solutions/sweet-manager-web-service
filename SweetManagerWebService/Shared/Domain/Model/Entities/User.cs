8fa13eb6060a3e97cc0f6ade96c3bd9075cf1c34
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-13 12:01:08 -0500

    fix: changed user entity structure in shared bounded context.

--- SweetManagerWebService/Shared/Domain/Model/Entities/User.cs
+++ SweetManagerWebService/Shared/Domain/Model/Entities/User.cs
@@ -3,20 +3,16 @@ using SweetManagerWebService.Shared.Domain.Model.ValueObjects;
 
 namespace SweetManagerWebService.Shared.Domain.Model.Entities;
 
-public class User(string? username, string email, Role? role, string name, string surname, string phoneNumber)
+public class User(string email, string name, string surname, string phoneNumber)
 {
     public int Id { get; set; }
     
-    public string? Username { get; private set; } = username;
-
     public string Email { get; private set; } = email;
-
-    public Role? Role { get; private set; } = role;
 
     public CompleteName Name { get; private set; } = new CompleteName(name, surname);
 
     public string PhoneNumber { get; private set; } = phoneNumber;
 
-    public User() : this(string.Empty, string.Empty, new Role(), string.Empty, string.Empty, string.Empty){ }
+    public User() : this(string.Empty, string.Empty,  string.Empty, string.Empty){ }
     
 }
