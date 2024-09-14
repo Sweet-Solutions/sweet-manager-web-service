2ea11effb43bf0c241e14011c99f9dd73f402e6b
aaron acuña <aaronacu_85@hotmail.com>
2024-09-13 20:07:06 -0500

    feat(monitoring): added repositories for booking and room.

---
+++ SweetManagerWebService/Monitoring/Domain/Repositories/IBookingRepository.cs
@@ -0,0 +1,11 @@
+﻿using sweetmanager.API.Shared.Domain.Repositories;
+using SweetManagerWebService.Monitoring.Domain.Model.Aggregates;
+
+namespace SweetManagerWebService.Monitoring.Domain.Repositories
+{
+    public interface IBookingRepository :
+        IBaseRepository<Booking>
+    {
+        
+    }
+}
