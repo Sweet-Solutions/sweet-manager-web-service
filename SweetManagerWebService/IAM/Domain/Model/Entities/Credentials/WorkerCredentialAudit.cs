4d56471d75a197430769888b890cbf96c2a128e1
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-13 21:16:52 -0500

    feat: added auditory for owner and worker credential.

---
+++ SweetManagerWebService/IAM/Domain/Model/Entities/Credentials/WorkerCredentialAudit.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+
+namespace SweetManagerWebService.IAM.Domain.Model.Entities.Credentials;
+
+public class WorkerCredentialAudit : IEntityWithCreatedUpdatedDate
+{
+    [NotMapped]
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    
+    [NotMapped]
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
