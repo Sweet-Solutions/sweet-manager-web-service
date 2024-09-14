bc3bce4d1fd073b9fc06532797d50971e639eb45
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-13 23:52:20 -0500

    feat(iam): added queries and repository for worker credential entity.

---
+++ SweetManagerWebService/IAM/Domain/Repositories/Credential/IWorkerCredentialRepository.cs
@@ -0,0 +1,9 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Entities.Credentials;
+
+namespace SweetManagerWebService.IAM.Domain.Repositories.Credential;
+
+public interface IWorkerCredentialRepository
+{
+    Task<WorkerCredential> FindByIdAsync(int id);
+    
+}
