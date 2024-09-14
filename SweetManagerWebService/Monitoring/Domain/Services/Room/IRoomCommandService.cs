95fba5f6fc9cdf74a8482aa29eb0200fde1fe75e
aaron acuña <aaronacu_85@hotmail.com>
2024-09-13 21:12:20 -0500

    feat(monitoring): added services for room.

---
+++ SweetManagerWebService/Monitoring/Domain/Services/Room/IRoomCommandService.cs
@@ -0,0 +1,13 @@
+﻿using SweetManagerWebService.Monitoring.Domain.Model.Commands.Room;
+
+namespace SweetManagerWebService.Monitoring.Domain.Services.Room
+{
+    public interface IRoomCommandService
+    {
+        Task<bool> Handle
+            (CreateRoomCommand command);
+
+        Task<bool> Handle
+            (UpdateRoomStateCommand command);
+    }
+}
---
+++ SweetManagerWebService/Monitoring/Domain/Services/Room/IRoomQueryService.cs
@@ -0,0 +1,13 @@
+﻿using SweetManagerWebService.Monitoring.Domain.Model.Queries.Room;
+
+namespace SweetManagerWebService.Monitoring.Domain.Services.Room
+{
+    public interface IRoomQueryService
+    {
+        Task<IEnumerable<Model.Aggregates.Room>> Handle
+            (GetAllRoomsQuery query);
+
+        Task<Model.Aggregates.Room?> Handle
+            (GetRoomByIdQuery query);
+    }
+}
