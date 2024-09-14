b8a431b34ea29ef6078f8ac2ee9cea6a6e70239e
aaron acuña <aaronacu_85@hotmail.com>
2024-09-13 21:55:21 -0500

    feat(monitoring): implemented services.

---
+++ SweetManagerWebService/Monitoring/Application/Internal/CommandServices/BookingCommandService.cs
@@ -0,0 +1,33 @@
+﻿using sweetmanager.API.Shared.Domain.Repositories;
+using SweetManagerWebService.Monitoring.Domain.Model.Commands.Booking;
+using SweetManagerWebService.Monitoring.Domain.Repositories;
+using SweetManagerWebService.Monitoring.Domain.Services.Booking;
+
+namespace SweetManagerWebService.Monitoring.Application.Internal.CommandServices
+{
+    public class BookingCommandService
+        (IBookingRepository bookingRepository,
+        IUnitOfWork unitOfWork) :
+        IBookingCommandService
+    {
+        public async Task<bool> Handle
+            (CreateBookingCommand command)
+        {
+            try
+            {
+                await bookingRepository
+                    .AddAsync(new(command));
+
+                await unitOfWork.CompleteAsync();
+
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+
+        public async Task<bool> Handle
+            (UpdateBookingStateCommand command) =>
+            await bookingRepository.UpdateBookingStateAsync
+            (command.Id, command.BookingState);
+    }
+}
---
+++ SweetManagerWebService/Monitoring/Application/Internal/CommandServices/RoomCommandService.cs
@@ -0,0 +1,33 @@
+﻿using sweetmanager.API.Shared.Domain.Repositories;
+using SweetManagerWebService.Monitoring.Domain.Model.Commands.Room;
+using SweetManagerWebService.Monitoring.Domain.Repositories;
+using SweetManagerWebService.Monitoring.Domain.Services.Room;
+
+namespace SweetManagerWebService.Monitoring.Application.Internal.CommandServices
+{
+    public class RoomCommandService
+        (IRoomRepository roomRepository,
+        IUnitOfWork unitOfWork) :
+        IRoomCommandService
+    {
+        public async Task<bool> Handle
+            (CreateRoomCommand command)
+        {
+            try
+            {
+                await roomRepository
+                    .AddAsync(new(command));
+
+                await unitOfWork.CompleteAsync();
+
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+
+        public async Task<bool> Handle
+            (UpdateRoomStateCommand command) =>
+            await roomRepository.UpdateRoomStateAsync
+            (command.Id, command.RoomState);
+    }
+}
