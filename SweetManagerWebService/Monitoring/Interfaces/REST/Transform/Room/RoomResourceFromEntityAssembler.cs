2af13492be7ea438773f60683b2c1255a55d7f0c
aaron acuña <aaronacu_85@hotmail.com>
2024-09-13 22:56:58 -0500

    feat(monitoring): added transform for room.

---
+++ SweetManagerWebService/Monitoring/Interfaces/REST/Transform/Room/RoomResourceFromEntityAssembler.cs
@@ -0,0 +1,12 @@
+﻿using SweetManagerWebService.Monitoring.Interfaces.REST.Resources.Room;
+
+namespace SweetManagerWebService.Monitoring.Interfaces.REST.Transform.Room
+{
+    public class RoomResourceFromEntityAssembler
+    {
+        public static RoomResource ToResourceFromEntity
+            (Domain.Model.Aggregates.Room entity) =>
+            new(entity.Id, entity.TypesRoomsId,
+                entity.HotelsId, entity.State);
+    }
+}
