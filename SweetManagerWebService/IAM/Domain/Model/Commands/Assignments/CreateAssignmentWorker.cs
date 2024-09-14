cf8ce11babbcba3e82ea3f26c070608474db7919
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-13 21:15:34 -0500

    feat: added command for assignment worker writing.

---
+++ SweetManagerWebService/IAM/Domain/Model/Commands/Assignments/CreateAssignmentWorker.cs
@@ -0,0 +1,4 @@
+﻿namespace SweetManagerWebService.IAM.Domain.Model.Commands.Assignments;
+
+public record CreateAssignmentWorker(int WorkersAreasId, int WorkersId, int AdminsId,
+    DateTime StartDate, DateTime FinalDate, string State);
