70dae7c0db862a2fbfc1884267cf79e8f43984b5
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-13 23:50:02 -0500

    feat(iam): added queries and repository for  assignment worker entity.

---
+++ SweetManagerWebService/IAM/Domain/Repositories/Assignments/IAssignmentWorkerRepository.cs
@@ -0,0 +1,15 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Entities.Assignments;
+
+namespace SweetManagerWebService.IAM.Domain.Repositories.Assignments;
+
+public interface IAssignmentWorkerRepository
+{
+    Task<AssignmentWorker> FindByIdAsync(int id);
+
+    Task<AssignmentWorker> FindByWorkerIdAsync(int workerId);
+
+    Task<AssignmentWorker> FindByAdminIdAsync(int adminId);
+
+    Task<AssignmentWorker> FindByWorkerAreaIdAsync(int workerAreaId);
+    
+}
---
+++ SweetManagerWebService/IAM/Domain/Services/Assignments/IAssingmentWorkerQueryService.cs
@@ -0,0 +1,17 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Commands.Assignments;
+using SweetManagerWebService.IAM.Domain.Model.Entities.Assignments;
+using SweetManagerWebService.IAM.Domain.Model.Queries;
+
+namespace SweetManagerWebService.IAM.Domain.Services.Assignments;
+
+public interface IAssignmentWorkerQueryService
+{
+    Task<AssignmentWorker> Handle(GetAssignmentWorkerByIdQuery query);
+
+    Task<AssignmentWorker> Handle(GetAssignmentWorkerByAdminIdQuery query);
+
+    Task<AssignmentWorker> Handle(GetAssignmentWorkerByWorkerIdQuery query);
+
+    Task<AssignmentWorker> Handle(GetAssignmentWorkerByWorkerAreaIdQuery query);
+
+}
