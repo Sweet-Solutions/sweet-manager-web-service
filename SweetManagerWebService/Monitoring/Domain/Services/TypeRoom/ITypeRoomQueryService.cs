5b854d3629551901a5ab9ad2315fa136e6ffda90
aaron acuña <aaronacu_85@hotmail.com>
2024-09-13 21:15:58 -0500

    feat(monitoring): added services for type room.

---
+++ SweetManagerWebService/Monitoring/Domain/Services/TypeRoom/ITypeRoomQueryService.cs
@@ -0,0 +1,13 @@
+﻿using SweetManagerWebService.Monitoring.Domain.Model.Queries.TypeRoom;
+
+namespace SweetManagerWebService.Monitoring.Domain.Services.TypeRoom
+{
+    public interface ITypeRoomQueryService
+    {
+        Task<IEnumerable<Model.Entities.TypeRoom>> Handle
+            (GetAllTypesRoomsQuery query);
+
+        Task<Model.Entities.TypeRoom?> Handle
+            (GetTypeRoomByIdQuery query);
+    }
+}
