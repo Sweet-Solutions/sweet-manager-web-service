d4d3422425e15e82d06c29e20f9703fdf56c44ed
aaron acuña <aaronacu_85@hotmail.com>
2024-09-13 20:50:56 -0500

    feat(monitoring): added command for booking.

---
+++ SweetManagerWebService/Monitoring/Domain/Model/Commands/Booking/CreateBookingCommand.cs
@@ -0,0 +1,10 @@
+﻿using SweetManagerWebService.Monitoring.Domain.Model.ValueObjects.Booking;
+
+namespace SweetManagerWebService.Monitoring.Domain.Model.Commands.Booking
+{
+    public record CreateBookingCommand
+        (int PaymentCustomerId, int RoomId,
+        string Description, DateTime StartDate,
+        DateTime FinalDate, decimal PriceRoom,
+        int NightCount, EBookingState BookingState);
+}
---
+++ SweetManagerWebService/Monitoring/Domain/Model/Commands/Booking/UpdateBookingStateCommand.cs
@@ -0,0 +1,7 @@
+﻿using SweetManagerWebService.Monitoring.Domain.Model.ValueObjects.Booking;
+
+namespace SweetManagerWebService.Monitoring.Domain.Model.Commands.Booking
+{
+    public record UpdateBookingStateCommand
+        (int Id, EBookingState BookingState);
+}
