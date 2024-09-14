58e0e61ca3855a2029a7ca9c03ae475f01cbe626
aaron acuña <aaronacu_85@hotmail.com>
2024-09-13 22:55:40 -0500

    feat(monitoring): added resource of entity for booking.

---
+++ SweetManagerWebService/Monitoring/Interfaces/REST/Resources/Booking/BookingResource.cs
@@ -0,0 +1,9 @@
+﻿namespace SweetManagerWebService.Monitoring.Interfaces.REST.Resources.Booking
+{
+    public record BookingResource
+        (int Id, int PaymentCustomerId, int RoomId,
+        string Description, DateTime StartDate,
+        DateTime FinalDate, decimal PriceRoom,
+        int NightCount, decimal Amount,
+        string BookingState);
+}
