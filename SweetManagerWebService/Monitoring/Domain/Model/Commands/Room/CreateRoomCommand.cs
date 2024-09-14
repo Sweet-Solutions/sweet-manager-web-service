412140ef865da68d75f335952ad5a6f8ba19c6fe
aaron acuña <aaronacu_85@hotmail.com>
2024-09-13 20:51:23 -0500

    feat(monitoring): added command for room.

---
+++ SweetManagerWebService/Monitoring/Domain/Model/Commands/Room/CreateRoomCommand.cs
@@ -0,0 +1,8 @@
+﻿using SweetManagerWebService.Monitoring.Domain.Model.ValueObjects.Room;
+
+namespace SweetManagerWebService.Monitoring.Domain.Model.Commands.Room
+{
+    public record CreateRoomCommand
+        (int TypeRoomId, int HotelId,
+        ERoomState RoomState);
+}
