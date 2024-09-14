cac6f1a985a39d9b32ed8d1b10c23062ee8f6413
aaron acuña <aaronacu_85@hotmail.com>
2024-09-13 22:56:37 -0500

    feat(monitoring): added transform for booking.

---
+++ SweetManagerWebService/Monitoring/Interfaces/REST/Transform/Booking/UpdateBookingCommandFromResourceAssembler.cs
@@ -0,0 +1,14 @@
+﻿using SweetManagerWebService.Monitoring.Domain.Model.Commands.Booking;
+using SweetManagerWebService.Monitoring.Domain.Model.ValueObjects.Booking;
+using SweetManagerWebService.Monitoring.Interfaces.REST.Resources.Booking;
+
+namespace SweetManagerWebService.Monitoring.Interfaces.REST.Transform.Booking
+{
+    public class UpdateBookingCommandFromResourceAssembler
+    {
+        public static UpdateBookingStateCommand ToCommandFromResource
+            (UpdateBookingStateResource resource) =>
+            new(resource.Id, Enum.Parse<EBookingState>
+                (resource.BookingState));
+    }
+}
