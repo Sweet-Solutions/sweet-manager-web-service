62fdf700c1efe636cd14137aeb5e5616b2982c9e
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-13 21:15:05 -0500

    feat: added assignment worker and his audit.

--- SweetManagerWebService/IAM/Domain/Model/Entities/Assignments/AssignmentWorker.cs
+++ SweetManagerWebService/IAM/Domain/Model/Entities/Assignments/AssignmentWorker.cs
@@ -1,17 +1,18 @@
 ﻿using SweetManagerWebService.IAM.Domain.Model.Aggregates;
+using SweetManagerWebService.IAM.Domain.Model.Commands.Assignments;
 using SweetManagerWebService.IAM.Domain.Model.Entities.Roles;
 
 namespace SweetManagerWebService.IAM.Domain.Model.Entities.Assignments
 {
-    public partial class AssignmentWorker
+    public partial class AssignmentWorker(CreateAssignmentWorker command)
     {
         public int Id { get; }
-        public int WorkersAreasId { get; set; }
-        public int WorkersId { get; set; }
-        public int AdminsId { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime FinalDate { get; set; }
-        public string State { get; set; } = null!;
+        public int WorkersAreasId { get; private set; } = command.WorkersAreasId;
+        public int WorkersId { get; private set; } = command.WorkersId;
+        public int AdminsId { get; private set; } = command.AdminsId;
+        public DateTime StartDate { get; private set; } = command.StartDate;
+        public DateTime FinalDate { get; private set; } = command.FinalDate;
+        public string State { get; private set; } = command.State;
 
         public virtual Admin Admin { get; } = null!;
         public virtual Worker Worker { get; } = null!;
---
+++ SweetManagerWebService/IAM/Domain/Model/Entities/Assignments/AssignmentWorkerAudit.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+
+namespace SweetManagerWebService.IAM.Domain.Model.Entities.Assignments;
+
+public partial class AssignmentWorker : IEntityWithCreatedUpdatedDate
+{
+    [NotMapped]
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    
+    [NotMapped]
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
