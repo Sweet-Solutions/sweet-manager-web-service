e9c0db782a6065e734d328bc0a416efb5598845e
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-13 21:14:32 -0500

    feat: added admin credential and auditory.

--- SweetManagerWebService/IAM/Domain/Model/Entities/Credentials/AdminCredentialAudit.cs
+++ SweetManagerWebService/IAM/Domain/Model/Entities/Credentials/AdminCredentialAudit.cs
@@ -1,9 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
 
-namespace SweetManagerWebService.IAM.Domain.Model.Aggregates;
+namespace SweetManagerWebService.IAM.Domain.Model.Entities.Credentials;
 
-public partial class Admin : IEntityWithCreatedUpdatedDate
+public class AdminCredentialAudit : IEntityWithCreatedUpdatedDate
 {
     [NotMapped]
     [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
