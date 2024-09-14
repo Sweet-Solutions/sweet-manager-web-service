d67ad427a7af5b71bc44d619ca95b870c73d20c4
aaron acuña <aaronacu_85@hotmail.com>
2024-09-13 20:52:51 -0500

    feat(monitoring): implemented commands for booking and room.

--- SweetManagerWebService/Monitoring/Domain/Model/Aggregates/Room.cs
+++ SweetManagerWebService/Monitoring/Domain/Model/Aggregates/Room.cs
@@ -1,4 +1,6 @@
-﻿using SweetManagerWebService.Monitoring.Domain.Model.Entities;
+﻿using SweetManagerWebService.Monitoring.Domain.Model.Commands.Room;
+using SweetManagerWebService.Monitoring.Domain.Model.Entities;
+using SweetManagerWebService.Monitoring.Domain.Model.ValueObjects.Room;
 using SweetManagerWebService.Profiles.Domain.Model.Entities;
 
 namespace SweetManagerWebService.Monitoring.Domain.Model.Aggregates
@@ -14,5 +16,33 @@ namespace SweetManagerWebService.Monitoring.Domain.Model.Aggregates
         public virtual TypeRoom TypeRoom { get; } = null!;
 
         public virtual ICollection<Booking> Bookings { get; } = [];
+
+        public Room()
+        {
+            this.TypesRoomsId = 0;
+            this.HotelsId = 0;
+            this.State = string.Empty;
+        }
+        public Room
+            (int typeRoomId, int hotelId,
+            ERoomState roomState)
+        {
+            this.TypesRoomsId = typeRoomId;
+            this.HotelsId = hotelId;
+            this.State = roomState.ToString();
+        }
+        public Room
+            (CreateRoomCommand command)
+        {
+            this.TypesRoomsId = command.TypeRoomId;
+            this.HotelsId = command.HotelId;
+            this.State = command.RoomState.ToString();
+        }
+        public Room
+            (UpdateRoomStateCommand command)
+        {
+            this.Id = command.Id;
+            this.State = command.RoomState.ToString();
+        }
     }
 }
