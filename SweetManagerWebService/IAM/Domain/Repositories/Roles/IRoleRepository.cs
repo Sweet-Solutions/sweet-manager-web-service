47466c28987535a747692581256be29ca3654148
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-13 23:51:16 -0500

    feat(iam): added queries and repository for role entity.

---
+++ SweetManagerWebService/IAM/Domain/Repositories/Roles/IRoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Entities.Roles;
+
+namespace SweetManagerWebService.IAM.Domain.Repositories.Roles;
+
+public interface IRoleRepository
+{
+    Task<IEnumerable<Role>> FindAllAsync();
+
+    Task<Role> FindByName(int name);
+
+    Task<int> FindIdByName(int name);
+}
---
+++ SweetManagerWebService/IAM/Domain/Services/Roles/IRoleCommandService.cs
@@ -0,0 +1,8 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Commands.Role;
+
+namespace SweetManagerWebService.IAM.Domain.Services.Roles;
+
+public interface IRoleCommandService
+{
+    Task<bool> Handle(SeedRolesCommand command);
+}
