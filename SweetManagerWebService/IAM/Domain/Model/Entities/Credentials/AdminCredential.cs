229a8a9012128d098b8b77c48c475d75e80e8d66
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-13 22:12:15 -0500

    feat: added admin credentials entities.

--- SweetManagerWebService/IAM/Domain/Model/Entities/Credentials/AdminCredential.cs
+++ SweetManagerWebService/IAM/Domain/Model/Entities/Credentials/AdminCredential.cs
@@ -6,6 +6,7 @@ namespace  SweetManagerWebService.IAM.Domain.Model.Entities.Credentials
     public partial class AdminCredential
     {
         public int AdminsId { get; set; }
+        
         public string Code { get; set; } = null!;
 
         public virtual Admin Admin { get; } = null!;
