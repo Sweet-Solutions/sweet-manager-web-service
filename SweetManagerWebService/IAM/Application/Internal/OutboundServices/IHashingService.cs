4c81c4b9b8e7e3c3ae2471663945edfca84c61de
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-14 20:15:58 -0500

    feat(iam): added hashing and token service.

---
+++ SweetManagerWebService/IAM/Application/Internal/OutboundServices/IHashingService.cs
@@ -0,0 +1,10 @@
+﻿namespace SweetManagerWebService.IAM.Application.Internal.OutboundServices;
+
+public interface IHashingService
+{
+    string CreateSalt();
+
+    string HashCode(string code, string salt);
+
+    public bool VerifyHash(string code, string salt, string hash);
+}
