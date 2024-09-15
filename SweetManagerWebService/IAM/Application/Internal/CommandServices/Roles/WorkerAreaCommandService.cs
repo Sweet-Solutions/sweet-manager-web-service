a22fd4c10f27e73b2aad1977627a033705d4526f
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-15 02:17:34 -0500

    fix(iam): fixed command service for worker area entity.

--- SweetManagerWebService/IAM/Application/Internal/CommandServices/Roles/WorkerAreaCommandService.cs
+++ SweetManagerWebService/IAM/Application/Internal/CommandServices/Roles/WorkerAreaCommandService.cs
@@ -1,6 +1,33 @@
-﻿namespace SweetManagerWebService.IAM.Application.Internal.CommandServices.Roles;
+﻿using sweetmanager.API.Shared.Domain.Repositories;
+using SweetManagerWebService.IAM.Domain.Model.Commands.Role;
+using SweetManagerWebService.IAM.Domain.Model.Entities.Roles;
+using SweetManagerWebService.IAM.Domain.Model.Exceptions;
+using SweetManagerWebService.IAM.Domain.Model.Queries;
+using SweetManagerWebService.IAM.Domain.Repositories.Roles;
+using SweetManagerWebService.IAM.Domain.Services.Roles;
 
-public class WorkerAreaCommandService
+namespace SweetManagerWebService.IAM.Application.Internal.CommandServices.Roles;
+
+public class WorkerAreaCommandService(IUnitOfWork unitOfWork, IWorkerAreaRepository workerAreaRepository, IWorkerAreaQueryService workerAreaQueryService) : IWorkerAreaCommandService
 {
-    
+    public async Task<bool> Handle(CreateWorkAreaCommand command)
+    {
+        try
+        {
+            
+            if (await workerAreaQueryService.Handle(new GetWorkerAreaByNameAndHotelIdQuery(command.Name, command.HotelId)) is
+                not null)
+                throw new WorkAreaWithTheGivenNameAlreadyExistException();
+            
+            await workerAreaRepository.AddAsync(new WorkerArea(command.Name));
+
+            await unitOfWork.CompleteAsync();
+            
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
 }
