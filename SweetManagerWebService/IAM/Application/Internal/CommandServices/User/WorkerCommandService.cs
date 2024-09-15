d59fc55b4781dcc54db507b21208495239efbe1b
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-15 02:15:12 -0500

    fix(iam): fixed command and query services for worker aggregate.

--- SweetManagerWebService/IAM/Application/Internal/CommandServices/User/WorkerCommandService.cs
+++ SweetManagerWebService/IAM/Application/Internal/CommandServices/User/WorkerCommandService.cs
@@ -1,6 +1,109 @@
-﻿namespace SweetManagerWebService.IAM.Application.Internal.CommandServices.User;
+﻿using sweetmanager.API.Shared.Domain.Repositories;
+using SweetManagerWebService.IAM.Application.Internal.OutboundServices;
+using SweetManagerWebService.IAM.Domain.Model.Aggregates;
+using SweetManagerWebService.IAM.Domain.Model.Commands;
+using SweetManagerWebService.IAM.Domain.Model.Commands.Authentication.User;
+using SweetManagerWebService.IAM.Domain.Model.Exceptions;
+using SweetManagerWebService.IAM.Domain.Repositories.Credential;
+using SweetManagerWebService.IAM.Domain.Repositories.Users;
+using SweetManagerWebService.IAM.Domain.Services.Users.Worker;
 
-public class WorkerCommandService
+namespace SweetManagerWebService.IAM.Application.Internal.CommandServices.User;
+
+public class WorkerCommandService(IUnitOfWork unitOfWork, 
+    IWorkerRepository workerRepository, 
+    IHashingService hashingService,
+    IWorkerCredentialRepository workerCredentialRepository,
+    ITokenService tokenService
+    ) : IWorkerCommandService
 {
-    
+    public async Task<bool> Handle(SignUpUserCommand command)
+    {
+        try
+        {
+            if (await workerRepository.FindByEmail(command.Email) is not null)
+                throw new EmailAlreadyExistException();
+            
+            // Add Worker
+
+            await workerRepository.AddAsync(new Worker(command.Id, command.Username, command.Name, command.Surname,
+                command.RolesId, command.Phone,
+                command.Email, command.State));
+
+            await unitOfWork.CompleteAsync();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"An error occurred while creating the user: {ex.Message}");
+        }
+    }
+
+    public async Task<bool> Handle(UpdateUserCommand command)
+    {
+        try
+        {
+            var result = false;
+            
+            if (command.Change == "PHONE")
+            {
+                await workerRepository.ExecuteUpdateWorkerPhoneAsync(Convert.ToInt32(command.Value), command.Id);
+
+                await unitOfWork.CompleteAsync();
+
+                result = true;
+            }
+            else
+            {
+                await workerRepository.ExecuteUpdateWorkerEmailAsync(command.Value, command.Id);
+
+                await unitOfWork.CompleteAsync();
+
+                result = true;
+            }
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"An error occurred while updating the user: {ex.Message}");
+        }
+    }
+
+    public async Task<dynamic?> Handle(SignInCommand command)
+    {
+        try
+        {
+            var user = await workerRepository.FindByEmail(command.Email);
+
+            if (user is null)
+                throw new EmailDoesntExistException();
+
+            var userCredential = await workerCredentialRepository.FindByWorkersIdAsync(user.Id);
+
+            if (!hashingService.VerifyHash(command.Password, userCredential!.Code[..24], userCredential!.Code[24..]))
+                throw new InvalidPasswordException();
+
+            var hotel = workerRepository.FindHotelIdByWorkerId(user.Id);
+
+            var token = tokenService.GenerateToken(new
+            {
+                Id = user.Id,
+                PasswordHash = userCredential.Code,
+                Role = "ROLE_WORKER",
+                Hotel = hotel
+            });
+
+            return new
+            {
+                User = user,
+                Token = token
+            };
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
 }
--- SweetManagerWebService/IAM/Infrastructure/Persistence/EFC/Repositories/User/WorkerRepository.cs
+++ SweetManagerWebService/IAM/Infrastructure/Persistence/EFC/Repositories/User/WorkerRepository.cs
@@ -1,4 +1,5 @@
-﻿using SweetManagerWebService.Communication.Domain.Model.Aggregates;
+﻿using Microsoft.EntityFrameworkCore;
+using SweetManagerWebService.Communication.Domain.Model.Aggregates;
 using SweetManagerWebService.IAM.Domain.Model.Aggregates;
 using SweetManagerWebService.IAM.Domain.Repositories.Users;
 using SweetManagerWebService.Profiles.Domain.Model.Entities;
@@ -42,5 +43,25 @@ public class WorkerRepository(SweetManagerContext context) : BaseRepository<Work
             where wo.Email.Equals(email)
             select wo.Id
         ).FirstOrDefault());
-    
+
+    public async Task<bool> ExecuteUpdateWorkerEmailAsync(string email, int id)
+        => await Context.Set<Worker>().Where(w => w.Id.Equals(id))
+            .ExecuteUpdateAsync(w => w.SetProperty(p => p.Email, email)) > 0;
+
+    public async Task<bool> ExecuteUpdateWorkerPhoneAsync(int phone, int id)
+        => await Context.Set<Worker>().Where(w => w.Id.Equals(id))
+            .ExecuteUpdateAsync(w => w.SetProperty(p => p.Phone, phone)) > 0;
+
+    public async Task<int?> FindHotelIdByWorkerId(int id)
+        => await Task.Run(() => (
+            from wo in Context.Set<Worker>().ToList()
+            join nt in Context.Set<Notification>().ToList()
+                on wo.Id equals nt.WorkersId
+            join ow in Context.Set<Owner>().ToList()
+                on nt.OwnersId equals ow.Id
+            join ho in Context.Set<Hotel>().ToList()
+                on ow.Id equals ho.OwnersId
+            where wo.Id.Equals(id)
+            select ho.Id
+        ).FirstOrDefault());
 }
