26f3f806db8121826d37787b678dd7fb6b18ec69
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-15 02:14:03 -0500

    fix(iam): fixed query and repository service for worker area entity.

--- SweetManagerWebService/IAM/Application/Internal/QueryServices/Roles/WorkerAreaQueryService.cs
+++ SweetManagerWebService/IAM/Application/Internal/QueryServices/Roles/WorkerAreaQueryService.cs
@@ -1,6 +1,24 @@
-﻿namespace SweetManagerWebService.IAM.Application.Internal.QueryServices.Roles;
+﻿using SweetManagerWebService.IAM.Domain.Model.Entities.Roles;
+using SweetManagerWebService.IAM.Domain.Model.Queries;
+using SweetManagerWebService.IAM.Domain.Repositories.Roles;
+using SweetManagerWebService.IAM.Domain.Services.Roles;
 
-public class WorkerAreaQueryService
+namespace SweetManagerWebService.IAM.Application.Internal.QueryServices.Roles;
+
+public class WorkerAreaQueryService(IWorkerAreaRepository workerAreaRepository) : IWorkerAreaQueryService
 {
-    
+    public async Task<IEnumerable<WorkerArea>> Handle(GetAllWorkerAreasByHotelIdQuery query)
+    {
+        return await workerAreaRepository.FindAllAsync(query.HotelId);
+    }
+
+    public async Task<WorkerArea?> Handle(GetWorkerAreaByNameAndHotelIdQuery query)
+    {
+        return await workerAreaRepository.FindByNameAsync(query.Name, query.HotelId);
+    }
+
+    public async Task<int?> Handle(GetWorkerAreaIdByRoleNameAndHotelIdQuery query)
+    {
+        return await workerAreaRepository.FindIdByNameAsync(query.Name, query.HotelId);
+    }
 }
--- SweetManagerWebService/IAM/Infrastructure/Persistence/EFC/Repositories/Roles/WorkerAreaRepository.cs
+++ SweetManagerWebService/IAM/Infrastructure/Persistence/EFC/Repositories/Roles/WorkerAreaRepository.cs
@@ -1,6 +1,9 @@
-﻿using SweetManagerWebService.IAM.Domain.Model.Aggregates;
+﻿using SweetManagerWebService.Communication.Domain.Model.Aggregates;
+using SweetManagerWebService.IAM.Domain.Model.Aggregates;
+using SweetManagerWebService.IAM.Domain.Model.Entities.Assignments;
 using SweetManagerWebService.IAM.Domain.Model.Entities.Roles;
 using SweetManagerWebService.IAM.Domain.Repositories.Roles;
+using SweetManagerWebService.Profiles.Domain.Model.Entities;
 using SweetManagerWebService.Shared.Infrastructure.Persistence.EFC.Configuration;
 using SweetManagerWebService.Shared.Infrastructure.Persistence.EFC.Repositories;
 
@@ -8,24 +11,55 @@ namespace SweetManagerWebService.IAM.Infrastructure.Persistence.EFC.Repositories
 
 public class WorkerAreaRepository(SweetManagerContext context) : BaseRepository<WorkerArea>(context), IWorkerAreaRepository
 {
-    public async Task<IEnumerable<WorkerArea>> FindAllAsync()
+    public async Task<IEnumerable<WorkerArea>> FindAllAsync(int hotelId)
         => await Task.Run(() => (
             from wa in Context.Set<WorkerArea>().ToList()
+            join aw in Context.Set<AssignmentWorker>().ToList()
+                on wa.Id equals aw.WorkersAreasId
+            join ad in Context.Set<Admin>().ToList()
+                on aw.AdminsId equals ad.Id
+            join nt in Context.Set<Notification>().ToList()
+                on ad.Id equals nt.AdminsId
+            join ow in Context.Set<Owner>().ToList()
+                on nt.OwnersId equals ow.Id
+            join ho in Context.Set<Hotel>().ToList()
+                on ow.Id equals ho.OwnersId
+            where ho.Id.Equals(hotelId)
             select wa
         ).ToList());
 
-    public async Task<WorkerArea?> FindByNameAsync(string name)
+    public async Task<WorkerArea?> FindByNameAsync(string name, int hotelId)
         => await Task.Run(() => (
             from wa in Context.Set<WorkerArea>().ToList()
-            where wa.Name.Equals(name)
+            join aw in Context.Set<AssignmentWorker>().ToList()
+                on wa.Id equals aw.WorkersAreasId
+            join ad in Context.Set<Admin>().ToList()
+                on wa.Id equals ad.Id
+            join nt in Context.Set<Notification>().ToList()
+                on ad.Id equals nt.AdminsId
+            join ow in Context.Set<Owner>().ToList()
+                on nt.OwnersId equals ow.Id
+            join ho in Context.Set<Hotel>().ToList()
+                on ow.Id equals ho.OwnersId
+            where wa.Name == name && ho.Id == hotelId
             select wa
         ).FirstOrDefault());
-    
-    public async Task<int?> FindIdByNameAsync(string name)
+
+    public async Task<int?> FindIdByNameAsync(string name, int hotelId)
         => await Task.Run(() => (
-            from wa in Context.Set<Worker>().ToList()
-            where wa.Name.Equals(name)
+            from wa in Context.Set<WorkerArea>().ToList()
+            join aw in Context.Set<AssignmentWorker>().ToList()
+                on wa.Id equals aw.WorkersAreasId
+            join ad in Context.Set<Admin>().ToList()
+                on wa.Id equals ad.Id
+            join nt in Context.Set<Notification>().ToList()
+                on ad.Id equals nt.AdminsId
+            join ow in Context.Set<Owner>().ToList()
+                on nt.OwnersId equals ow.Id
+            join ho in Context.Set<Hotel>().ToList()
+                on ow.Id equals ho.OwnersId
+            where wa.Name == name && ho.Id == hotelId
             select wa.Id
         ).FirstOrDefault());
-
+    
 }
