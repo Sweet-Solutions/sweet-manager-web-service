19226b37bbde87945f3a463e0c98d5f6ee0339c6
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-14 20:17:25 -0500

    feat(iam): added query and command services for worker aggregate.

---
+++ SweetManagerWebService/IAM/Domain/Repositories/Users/IWorkerRepository.cs
@@ -0,0 +1,14 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Aggregates;
+
+namespace SweetManagerWebService.IAM.Domain.Repositories.Users;
+
+public interface IWorkerRepository
+{
+    Task<IEnumerable<Worker>> FindAllByHotelId(int hotelId);
+
+    Task<Worker?> FindById(int id);
+
+    Task<Worker?> FindByEmail(string email);
+
+    Task<int?> FindIdByEmail(string email);
+}
---
+++ SweetManagerWebService/IAM/Domain/Services/Users/Worker/IWorkerCommandService.cs
@@ -0,0 +1,10 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Commands.Authentication.User;
+
+namespace SweetManagerWebService.IAM.Domain.Services.Users.Worker;
+
+public interface IWorkerCommandService
+{
+    Task<bool> Handle(SignUpUserCommand command);
+
+    Task<bool> Handle(UpdateUserCommand command);
+}
