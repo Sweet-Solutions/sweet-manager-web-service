118e86f20e5a8d7f1cd90e3ac407f431b96d18f6
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-15 21:47:13 -0500

    feat(iam): added authenticated user resource.

---
+++ SweetManagerWebService/IAM/Interfaces/REST/Transform/Authentication/User/AuthenticatedUserResourceFromEntityAssembler.cs
@@ -0,0 +1,11 @@
+﻿using SweetManagerWebService.IAM.Interfaces.REST.Resource.Authentication.User;
+
+namespace SweetManagerWebService.IAM.Interfaces.REST.Transform.Authentication.User;
+
+public static class AuthenticatedUserResourceFromEntityAssembler
+{
+    public static AuthenticatedUserResource ToResourceFromEntity(dynamic entity, string token)
+    {
+        return new AuthenticatedUserResource(entity.Id, entity.Username, token);
+    }
+}
