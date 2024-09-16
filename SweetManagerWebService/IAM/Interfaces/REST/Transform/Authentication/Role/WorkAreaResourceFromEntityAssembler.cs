d2dccb12135dbf52a80cd1fe36644ba391b12313
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-15 22:01:59 -0500

    feat(iam): added resource and transformer for worker area entity.

---
+++ SweetManagerWebService/IAM/Interfaces/REST/Transform/Authentication/Role/WorkAreaResourceFromEntityAssembler.cs
@@ -0,0 +1,12 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Entities.Roles;
+using SweetManagerWebService.IAM.Interfaces.REST.Resource.Authentication.Role;
+
+namespace SweetManagerWebService.IAM.Interfaces.REST.Transform.Authentication.Role;
+
+public static class WorkAreaResourceFromEntityAssembler
+{
+    public static WorkAreaResource ToResourceFromEntity(WorkerArea entity)
+    {
+        return new(entity.Id, entity.Name);
+    }
+}
