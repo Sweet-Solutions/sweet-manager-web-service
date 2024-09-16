0d405b4266dc94ee4a7625b6bb861b69fc44d7bc
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-15 22:03:19 -0500

    chore(iam): updated injections and scoped.

--- SweetManagerWebService/Program.cs
+++ SweetManagerWebService/Program.cs
@@ -1,7 +1,41 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using sweetmanager.API.Shared.Domain.Repositories;
+using SweetManagerWebService.IAM.Application.Internal.CommandServices.Assignments;
+using SweetManagerWebService.IAM.Application.Internal.CommandServices.Credential;
+using SweetManagerWebService.IAM.Application.Internal.CommandServices.Roles;
+using SweetManagerWebService.IAM.Application.Internal.CommandServices.User;
+using SweetManagerWebService.IAM.Application.Internal.OutboundServices;
+using SweetManagerWebService.IAM.Application.Internal.QueryServices.Assignments;
+using SweetManagerWebService.IAM.Application.Internal.QueryServices.Credential;
+using SweetManagerWebService.IAM.Application.Internal.QueryServices.Roles;
+using SweetManagerWebService.IAM.Application.Internal.QueryServices.User;
+using SweetManagerWebService.IAM.Domain.Repositories.Assignments;
+using SweetManagerWebService.IAM.Domain.Repositories.Credential;
+using SweetManagerWebService.IAM.Domain.Repositories.Roles;
+using SweetManagerWebService.IAM.Domain.Repositories.Users;
+using SweetManagerWebService.IAM.Domain.Services.Assignments;
+using SweetManagerWebService.IAM.Domain.Services.Credential.Admin;
+using SweetManagerWebService.IAM.Domain.Services.Credential.Owner;
+using SweetManagerWebService.IAM.Domain.Services.Credential.Worker;
+using SweetManagerWebService.IAM.Domain.Services.Roles;
+using SweetManagerWebService.IAM.Domain.Services.Users.Admin;
+using SweetManagerWebService.IAM.Domain.Services.Users.Owner;
+using SweetManagerWebService.IAM.Domain.Services.Users.Worker;
+using SweetManagerWebService.IAM.Infrastructure.Hashing.Argon2Id.Services;
+using SweetManagerWebService.IAM.Infrastructure.Persistence.EFC.Repositories.Assignment;
+using SweetManagerWebService.IAM.Infrastructure.Persistence.EFC.Repositories.Credential;
+using SweetManagerWebService.IAM.Infrastructure.Persistence.EFC.Repositories.Roles;
+using SweetManagerWebService.IAM.Infrastructure.Persistence.EFC.Repositories.User;
+using SweetManagerWebService.IAM.Infrastructure.Population.Roles;
+using SweetManagerWebService.IAM.Infrastructure.Tokens.JWT.Configuration;
+using SweetManagerWebService.IAM.Infrastructure.Tokens.JWT.Services;
 using SweetManagerWebService.Shared.Infrastructure.Interfaces.ASP.Configuration;
 using SweetManagerWebService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using SweetManagerWebService.Shared.Infrastructure.Persistence.EFC.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,16 +47,35 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 #region Database Configuration
-
 // Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("SweetManager");
 
-builder.Services.AddDbContext<SweetManagerContext>(options =>
+var connectionStringFromEnvironment = Environment.GetEnvironmentVariable("SweetManagerDbConnection");
+
+if (connectionStringFromEnvironment != null)
 {
-    options.UseMySQL(builder.Configuration
-        .GetConnectionString("SweetManager"));
-});
+    connectionString = connectionStringFromEnvironment;
+}
+
+// Configure Database Context and Logging Levels
+builder.Services.AddDbContext<SweetManagerContext>(
+    options =>
+    {
+        if (connectionString != null)
+            if (builder.Environment.IsDevelopment())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            else if (builder.Environment.IsProduction())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Error)
+                    .EnableDetailedErrors();    
+    });
 
 #endregion
+
+
 
 #region OPENAPI Configuration
 // Configure Lowercase URLs
@@ -77,13 +130,114 @@ builder.Services.AddSwaggerGen(
 
 #endregion
 
+
+
+builder.Services.AddScoped<IAdminCommandService, AdminCommandService>();
+builder.Services.AddScoped<IAdminQueryService, AdminQueryService>();
+builder.Services.AddScoped<IAdminRepository, AdminRepository>();
+
+builder.Services.AddScoped<IOwnerCommandService, OwnerCommandService>();
+builder.Services.AddScoped<IOwnerQueryService, OwnerQueryService>();
+builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
+
+builder.Services.AddScoped<IWorkerCommandService, WorkerCommandService>();
+builder.Services.AddScoped<IWorkerQueryService, WorkerQueryService>();
+builder.Services.AddScoped<IWorkerRepository, WorkerRepository>();
+
+builder.Services.AddScoped<IAdminCredentialCommandService, AdminCredentialCommandService>();
+builder.Services.AddScoped<IAdminCredentialQueryService, AdminCredentialQueryService>();
+builder.Services.AddScoped<IAdminCredentialRepository, AdminCredentialRepository>();
+
+builder.Services.AddScoped<IOwnerCredentialCommandService, OwnerCredentialCommandService>();
+builder.Services.AddScoped<IOwnerCredentialQueryService, OwnerCredentialQueryService>();
+builder.Services.AddScoped<IOwnerCredentialRepository, OwnerCredentialRepository>();
+
+builder.Services.AddScoped<IWorkerCredentialCommandService, WorkerCredentialCommandService>();
+builder.Services.AddScoped<IWorkerCredentialQueryService, WorkerCredentialQueryService>();
+builder.Services.AddScoped<IWorkerCredentialRepository, WorkerCredentialRepository>();
+
+builder.Services.AddScoped<IRoleCommandService, RoleCommandService>();
+builder.Services.AddScoped<IRoleQueryService, RoleQueryService>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+
+builder.Services.AddScoped<IWorkerAreaCommandService, WorkerAreaCommandService>();
+builder.Services.AddScoped<IWorkerAreaQueryService, WorkerAreaQueryService>();
+builder.Services.AddScoped<IWorkerAreaRepository, WorkerAreaRepository>();
+
+builder.Services.AddScoped<IAssignmentWorkerCommandService, AssignmentWorkerCommandService>();
+builder.Services.AddScoped<IAssignmentWorkerQueryService, AssignmentWorkerQueryService>();
+builder.Services.AddScoped<IAssignmentWorkerRepository, AssignmentWorkerRepository>();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddScoped<RolesInitializer>();
+
 #region JWT Configuration
 
-// Empty for now
+var tokenSettings = builder.Configuration.GetSection("JwtSettings");
+
+builder.Services.Configure<TokenSettings>(tokenSettings);
+
+var secretKey = tokenSettings["SecretKey"];
+
+var audience = tokenSettings["Audience"];
+
+var issuer = tokenSettings["Issuer"];
+
+var securityKey = !string.IsNullOrEmpty(secretKey)
+    ? new SymmetricSecurityKey(Encoding.Default.GetBytes(secretKey))
+    : throw new ArgumentException("Secret key is null or empty");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = securityKey,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddScoped<IHashingService, HashingService>();
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services.AddTransient<TokenValidationHandler>();
+
+builder.Services.AddAuthorization();
 
 #endregion
 
 var app = builder.Build();
+
+#region Ensure Database Created (COMPILE AppDbContext)
+// Verify Database Objects are created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<SweetManagerContext>();
+    context.Database.EnsureCreated();
+}
+
+#endregion
+
+#region Run DatabaseInitializer
+using (var scope = app.Services.CreateScope())
+{
+    var initializer = scope.ServiceProvider.GetRequiredService<RolesInitializer>();
+
+    initializer.InitializeAsync().Wait();
+}
+#endregion
 
 // Configuration cors
 app.UseCors(
