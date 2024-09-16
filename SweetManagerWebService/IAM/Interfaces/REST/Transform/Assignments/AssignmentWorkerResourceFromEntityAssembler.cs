387e4330fed1a1d94205bfa780d4904a37ebb381
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-15 21:46:47 -0500

    feat(iam): added assignment workers resources.

---
+++ SweetManagerWebService/IAM/Interfaces/REST/Transform/Assignments/AssignmentWorkerResourceFromEntityAssembler.cs
@@ -0,0 +1,13 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Entities.Assignments;
+using SweetManagerWebService.IAM.Interfaces.REST.Resource.Assignments;
+
+namespace SweetManagerWebService.IAM.Interfaces.REST.Transform.Assignments;
+
+public static class AssignmentWorkerResourceFromEntityAssembler
+{
+    public static AssignmentWorkerResource ToResourceFromEntity(AssignmentWorker entity)
+    {
+        return new AssignmentWorkerResource(entity.Id, entity.WorkersAreasId, entity.WorkersId, entity.AdminsId,
+            entity.StartDate, entity.FinalDate, entity.State);
+    }
+}
