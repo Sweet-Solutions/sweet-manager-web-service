05abd9c454ad1943f34387936e94d6e975f13c39
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-15 21:48:10 -0500

    fxi(iam): fixed assignment workers for writing operations.

--- SweetManagerWebService/IAM/Application/Internal/CommandServices/Assignments/AssignmentWorkerCommandService.cs
+++ SweetManagerWebService/IAM/Application/Internal/CommandServices/Assignments/AssignmentWorkerCommandService.cs
@@ -1,6 +1,27 @@
-﻿namespace SweetManagerWebService.IAM.Application.Internal.CommandServices.Assignments;
+﻿using sweetmanager.API.Shared.Domain.Repositories;
+using SweetManagerWebService.IAM.Domain.Model.Commands.Assignments;
+using SweetManagerWebService.IAM.Domain.Model.Entities.Assignments;
+using SweetManagerWebService.IAM.Domain.Repositories.Assignments;
+using SweetManagerWebService.IAM.Domain.Services.Assignments;
 
-public class AssignmentWorkerCommandService
+namespace SweetManagerWebService.IAM.Application.Internal.CommandServices.Assignments;
+
+public class AssignmentWorkerCommandService(IAssignmentWorkerRepository assignmentWorkerRepository, 
+    IUnitOfWork unitOfWork) : IAssignmentWorkerCommandService
 {
-    
+    public async Task<bool> Handle(CreateAssignmentWorkerCommand command)
+    {
+        try
+        {
+            await assignmentWorkerRepository.AddAsync(new AssignmentWorker(command));
+
+            await unitOfWork.CompleteAsync();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
 }
--- SweetManagerWebService/IAM/Domain/Model/Entities/Assignments/AssignmentWorker.cs
+++ SweetManagerWebService/IAM/Domain/Model/Entities/Assignments/AssignmentWorker.cs
@@ -4,18 +4,30 @@ using SweetManagerWebService.IAM.Domain.Model.Entities.Roles;
 
 namespace SweetManagerWebService.IAM.Domain.Model.Entities.Assignments
 {
-    public partial class AssignmentWorker(CreateAssignmentWorker command)
+    public partial class AssignmentWorker
     {
         public int Id { get; }
-        public int WorkersAreasId { get; private set; } = command.WorkersAreasId;
-        public int WorkersId { get; private set; } = command.WorkersId;
-        public int AdminsId { get; private set; } = command.AdminsId;
-        public DateTime StartDate { get; private set; } = command.StartDate;
-        public DateTime FinalDate { get; private set; } = command.FinalDate;
-        public string State { get; private set; } = command.State;
+        public int WorkersAreasId { get; private set; }
+        public int WorkersId { get; private set; } 
+        public int AdminsId { get; private set; } 
+        public DateTime StartDate { get; private set; } 
+        public DateTime FinalDate { get; private set; }
+        public string State { get; private set; } 
 
         public virtual Admin Admin { get; } = null!;
         public virtual Worker Worker { get; } = null!;
         public virtual WorkerArea WorkerArea { get; } = null!;
+
+        public AssignmentWorker(CreateAssignmentWorkerCommand command)
+        {
+            WorkersAreasId = command.WorkersAreasId;
+            WorkersId = command.WorkersId;
+            AdminsId = command.WorkersId;
+            StartDate = command.StartDate;
+            FinalDate = command.FinalDate;
+            State = command.State;
+        }
+        
+        public AssignmentWorker() { }
     }
 }
