c24f3784dd5f4498d2c637dae0241e9ee217904c
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-15 21:50:20 -0500

    feat(iam): added observer service for populate role table.

---
+++ SweetManagerWebService/IAM/Infrastructure/Population/Roles/RolesInitializer.cs
@@ -0,0 +1,27 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Commands.Role;
+using SweetManagerWebService.IAM.Domain.Model.Queries;
+using SweetManagerWebService.IAM.Domain.Services.Roles;
+using SweetManagerWebService.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace SweetManagerWebService.IAM.Infrastructure.Population.Roles;
+
+public class RolesInitializer(IRoleCommandService roleCommandService,
+    IRoleQueryService roleQueryService,
+    SweetManagerContext context)
+{
+    public async Task InitializeAsync()
+    {
+        // Check if the role table is empty
+
+        var result = await roleQueryService.Handle(new GetAllRolesQuery());
+
+        if (!result.Any())
+        {
+            // Prepopulate the empty table
+
+            await roleCommandService.Handle(new SeedRolesCommand());
+        }
+        
+    }
+
+}
