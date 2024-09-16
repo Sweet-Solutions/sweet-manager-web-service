599c4c8a0bcad1878355ed3456580f2451155165
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-15 21:51:36 -0500

    feat(iam): added sign in and sign up transforms.

---
+++ SweetManagerWebService/IAM/Interfaces/REST/Transform/Authentication/User/SignUpUserCommandFromResourceAssembler.cs
@@ -0,0 +1,13 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Commands.Authentication.User;
+using SweetManagerWebService.IAM.Interfaces.REST.Resource.Authentication.User;
+
+namespace SweetManagerWebService.IAM.Interfaces.REST.Transform.Authentication.User;
+
+public static class SignUpUserCommandFromResourceAssembler
+{
+    public static SignUpUserCommand ToCommandFromResource(SignUpUserResource resource)
+    {
+        return new(resource.Id,  resource.Username, resource.Name, resource.Surname, resource.Email,
+            resource.Phone, resource.State, resource.Password);
+    }
+}
