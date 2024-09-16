6dd671c15ed4f42b66d8cf5a70e70899e7d9d018
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-15 21:47:42 -0500

    feat(iam): added assignment worker command and resources transformers.

---
+++ SweetManagerWebService/IAM/Interfaces/REST/Resource/Assignments/CreateAssignmentWorkerResource.cs
@@ -0,0 +1,4 @@
+﻿namespace SweetManagerWebService.IAM.Interfaces.REST.Resource.Assignments;
+
+public record CreateAssignmentWorkerResource(int WorkerAreasId, int WorkersId, int AdminsId,
+    string StartDate, string FinalDate, string State);
