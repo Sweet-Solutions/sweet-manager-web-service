9d7b9073df16f435c36f56317f0366b66121fb7e
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-15 22:12:59 -0500

    fix(iam): fixed attributes for role entity.

--- SweetManagerWebService/IAM/Domain/Model/Entities/Roles/Role.cs
+++ SweetManagerWebService/IAM/Domain/Model/Entities/Roles/Role.cs
@@ -6,11 +6,10 @@ namespace SweetManagerWebService.IAM.Domain.Model.Entities.Roles
     {
         public int Id { get; }
         
-        public int OwnersId { get; set; }
-        
         public string Name { get; private set; } = name;
         
-        public virtual Owner Owner { get; } = null!;
+        
+        public virtual ICollection<Owner> Owners { get; } = null!;
 
         public virtual ICollection<Admin> Admins { get; } = [];
         
