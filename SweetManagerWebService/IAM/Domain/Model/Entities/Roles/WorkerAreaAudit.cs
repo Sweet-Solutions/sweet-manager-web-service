1f7cfa726baeefea62f43bd37c7fff729b6b51e2
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-17 15:35:18 -0500

    fix(iam): refactor auditory for worker area entity.

--- SweetManagerWebService/IAM/Domain/Model/Entities/Roles/WorkerAreaAudit.cs
+++ SweetManagerWebService/IAM/Domain/Model/Entities/Roles/WorkerAreaAudit.cs
@@ -5,9 +5,7 @@ namespace SweetManagerWebService.IAM.Domain.Model.Entities.Roles;
 
 public class WorkerAreaAudit : IEntityWithCreatedUpdatedDate
 {
-    [NotMapped]
     [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
     
-    [NotMapped]
     [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
 }
