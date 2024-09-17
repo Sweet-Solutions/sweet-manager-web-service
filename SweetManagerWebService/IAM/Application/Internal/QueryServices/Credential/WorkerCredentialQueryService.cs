f947b5603998d599305bd2e203c1d16c3ebd8ab4
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-17 15:38:45 -0500

    fix(iam): refactor query service for worker credential entity.

--- SweetManagerWebService/IAM/Application/Internal/QueryServices/Credential/WorkerCredentialQueryService.cs
+++ SweetManagerWebService/IAM/Application/Internal/QueryServices/Credential/WorkerCredentialQueryService.cs
@@ -1,13 +1,12 @@
 ﻿using SweetManagerWebService.IAM.Domain.Model.Entities.Credentials;
 using SweetManagerWebService.IAM.Domain.Model.Queries;
+using SweetManagerWebService.IAM.Domain.Repositories.Credential;
 using SweetManagerWebService.IAM.Domain.Services.Credential.Worker;
 
 namespace SweetManagerWebService.IAM.Application.Internal.QueryServices.Credential;
 
-public class WorkerCredentialQueryService : IWorkerCredentialQueryService
+public class WorkerCredentialQueryService(IWorkerCredentialRepository workerCredentialRepository) : IWorkerCredentialQueryService
 {
-    public Task<WorkerCredential?> Handle(GetUserCredentialByIdQuery query)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<WorkerCredential?> Handle(GetUserCredentialByIdQuery query)
+        => await workerCredentialRepository.FindByWorkersIdAsync(query.UserId);
 }
