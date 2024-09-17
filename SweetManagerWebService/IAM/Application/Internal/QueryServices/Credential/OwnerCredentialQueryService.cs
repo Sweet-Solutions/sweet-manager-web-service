9b3f47bbdc796311f7406e209eb155fd1b6c973d
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-17 15:36:17 -0500

    fix(iam): refactor owner credential query service.

--- SweetManagerWebService/IAM/Application/Internal/QueryServices/Credential/OwnerCredentialQueryService.cs
+++ SweetManagerWebService/IAM/Application/Internal/QueryServices/Credential/OwnerCredentialQueryService.cs
@@ -1,13 +1,12 @@
 ﻿using SweetManagerWebService.IAM.Domain.Model.Entities.Credentials;
 using SweetManagerWebService.IAM.Domain.Model.Queries;
+using SweetManagerWebService.IAM.Domain.Repositories.Credential;
 using SweetManagerWebService.IAM.Domain.Services.Credential.Owner;
 
 namespace SweetManagerWebService.IAM.Application.Internal.QueryServices.Credential;
 
-public class OwnerCredentialQueryService : IOwnerCredentialQueryService
+public class OwnerCredentialQueryService(IOwnerCredentialRepository ownerCredentialRepository) : IOwnerCredentialQueryService
 {
-    public Task<OwnerCredential?> Handle(GetUserCredentialByIdQuery query)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<OwnerCredential?> Handle(GetUserCredentialByIdQuery query)
+        => await ownerCredentialRepository.FindByOwnersIdAsync(query.UserId);
 }
