b78f61841197be38638c2ae547e94ca33d74f4b4
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-17 15:36:56 -0500

    fix(iam): refactor query service for admin credential entity.

--- SweetManagerWebService/IAM/Application/Internal/QueryServices/Credential/AdminCredentialQueryService.cs
+++ SweetManagerWebService/IAM/Application/Internal/QueryServices/Credential/AdminCredentialQueryService.cs
@@ -1,13 +1,12 @@
 ﻿using SweetManagerWebService.IAM.Domain.Model.Entities.Credentials;
 using SweetManagerWebService.IAM.Domain.Model.Queries;
+using SweetManagerWebService.IAM.Domain.Repositories.Credential;
 using SweetManagerWebService.IAM.Domain.Services.Credential.Admin;
 
 namespace SweetManagerWebService.IAM.Application.Internal.QueryServices.Credential;
 
-public class AdminCredentialQueryService: IAdminCredentialQueryService
+public class AdminCredentialQueryService(IAdminCredentialRepository adminCredentialRepository): IAdminCredentialQueryService
 {
-    public Task<AdminCredential?> Handle(GetUserCredentialByIdQuery query)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<AdminCredential?> Handle(GetUserCredentialByIdQuery query)
+        => await adminCredentialRepository.FindByAdminsIdAsync(query.UserId);
 }
