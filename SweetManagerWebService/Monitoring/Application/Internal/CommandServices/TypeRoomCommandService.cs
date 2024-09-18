0536441d1caf7a37b476d2008addde674974406d
aaron acuña <aaronacu_85@hotmail.com>
2024-09-18 09:16:09 -0500

    feat(monitoring): implemented command service for type room.

---
+++ SweetManagerWebService/Monitoring/Application/Internal/CommandServices/TypeRoomCommandService.cs
@@ -0,0 +1,28 @@
+﻿using sweetmanager.API.Shared.Domain.Repositories;
+using SweetManagerWebService.Monitoring.Domain.Model.Commands.TypeRoom;
+using SweetManagerWebService.Monitoring.Domain.Repositories;
+using SweetManagerWebService.Monitoring.Domain.Services.TypeRoom;
+
+namespace SweetManagerWebService.Monitoring.Application.Internal.CommandServices
+{
+    public class TypeRoomCommandService
+        (ITypeRoomRepository typeRoomRepository,
+        IUnitOfWork unitOfWork) :
+        ITypeRoomCommandService
+    {
+        public async Task<bool> Handle
+            (CreateTypeRoomCommand command)
+        {
+            try
+            {
+                await typeRoomRepository
+                    .AddAsync(new(command));
+
+                await unitOfWork.CompleteAsync();
+
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+    }
+}
