44e197ac467d2db84b81faa00b6e813314a9e498
Fernando Salgado <u202212023@upc.edu.pe>
2024-09-18 01:04:22 -0500

    feat(resource): implemented resources.

---
+++ SweetManagerWebService/Communication/Interfaces/REST/Resources/Notification/NotificationResource.cs
@@ -0,0 +1,3 @@
+namespace SweetManagerWebService.Communication.Interfaces.REST.Resources.Notification;
+
+public record NotificationResource(int Id, int TypesNotificationsId, int OwnersId, int AdminsId, int WorkersId, string Title, string Description);
