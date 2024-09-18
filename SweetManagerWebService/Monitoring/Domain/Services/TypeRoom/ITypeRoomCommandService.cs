423bcf1f8476a3c81b856997e0d722d9b2d2d152
aaron acuña <aaronacu_85@hotmail.com>
2024-09-18 08:57:15 -0500

    feat(monitoring): added command service for type room.

---
+++ SweetManagerWebService/Monitoring/Domain/Services/TypeRoom/ITypeRoomCommandService.cs
@@ -0,0 +1,10 @@
+﻿using SweetManagerWebService.Monitoring.Domain.Model.Commands.TypeRoom;
+
+namespace SweetManagerWebService.Monitoring.Domain.Services.TypeRoom
+{
+    public interface ITypeRoomCommandService
+    {
+        Task<bool> Handle
+            (CreateTypeRoomCommand command);
+    }
+}
