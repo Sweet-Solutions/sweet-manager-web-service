5449742ce528d6ff582b30e119f4ecb63c47421f
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-19 15:51:31 -0500

    feat: added type report command service.

---
+++ SweetManagerWebService/ResourceManagement/Application/CommandService/TypeReportCommandService.cs
@@ -0,0 +1,27 @@
+﻿using sweetmanager.API.Shared.Domain.Repositories;
+using SweetManagerWebService.ResourceManagement.Domain.Model.Commands;
+using SweetManagerWebService.ResourceManagement.Domain.Model.Entities;
+using SweetManagerWebService.ResourceManagement.Domain.Model.ValueObjects;
+using SweetManagerWebService.ResourceManagement.Domain.Repositories;
+using SweetManagerWebService.ResourceManagement.Domain.Services.TypeReport;
+
+namespace SweetManagerWebService.ResourceManagement.Application.CommandService;
+
+public class TypeReportCommandService(ITypeReportRepository typeReportRepository,
+    IUnitOfWork unitOfWork) : ITypeReportCommandService
+{
+    public async Task<bool> Handle(SeedTypeReportsCommand command)
+    {
+        foreach (var typeReport in Enum.GetValues(typeof(ETypeReports)))
+        {
+            if (await typeReportRepository.FindByNameAsync(typeReport.ToString()!) is false)
+            {
+                await typeReportRepository.AddAsync(new TypeReport(typeReport.ToString()!));
+            }
+        }
+
+        await unitOfWork.CompleteAsync();
+        
+        return true;
+    }
+}
