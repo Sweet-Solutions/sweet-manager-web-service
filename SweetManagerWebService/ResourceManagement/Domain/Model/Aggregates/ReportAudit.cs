0a44156d4af8ad718d4f675fd318975629492483
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-19 15:49:01 -0500

    feat: added report auditory.

---
+++ SweetManagerWebService/ResourceManagement/Domain/Model/Aggregates/ReportAudit.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+
+namespace SweetManagerWebService.ResourceManagement.Domain.Model.Aggregates;
+
+public partial class Report : IEntityWithCreatedUpdatedDate
+{
+    [Column("created_at")] public DateTimeOffset? CreatedDate { get; set; }
+    
+    [Column("updated_at")] public DateTimeOffset? UpdatedDate { get; set; }
+}
