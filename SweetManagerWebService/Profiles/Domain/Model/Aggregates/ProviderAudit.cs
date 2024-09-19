ddf0c2f264f6232325ca6738499c6cb8afa65e9a
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-19 15:49:33 -0500

    feat: added provider auditory.

--- SweetManagerWebService/Profiles/Domain/Model/Aggregates/ProviderAudit.cs
+++ SweetManagerWebService/Profiles/Domain/Model/Aggregates/ProviderAudit.cs
@@ -5,9 +5,7 @@ namespace SweetManagerWebService.Profiles.Domain.Model.Aggregates;
 
 public partial class Provider : IEntityWithCreatedUpdatedDate
 {
-    [NotMapped]
     [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
     
-    [NotMapped]
     [Column("UpdateAt")] public DateTimeOffset? UpdatedDate { get; set; }
 }
