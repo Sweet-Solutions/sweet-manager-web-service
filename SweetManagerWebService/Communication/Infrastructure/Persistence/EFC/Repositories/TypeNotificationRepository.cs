3ea26bd893dbd714189732dcc6da4c70a544b091
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-19 15:49:57 -0500

    fix: updated type notification repository.

--- SweetManagerWebService/Communication/Infrastructure/Persistence/EFC/Repositories/TypeNotificationRepository.cs
+++ SweetManagerWebService/Communication/Infrastructure/Persistence/EFC/Repositories/TypeNotificationRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using SweetManagerWebService.Communication.Domain.Model.Entities;
 using SweetManagerWebService.Communication.Domain.Repositories;
 using SweetManagerWebService.Shared.Infrastructure.Persistence.EFC.Configuration;
@@ -5,13 +6,10 @@ using SweetManagerWebService.Shared.Infrastructure.Persistence.EFC.Repositories;
 
 namespace SweetManagerWebService.Communication.Infrastructure.Persistence.EFC.Repositories
 {
-    public class TypeNotificationRepository : BaseRepository<TypeNotification>, ITypeNotificationRepository
+    public class TypeNotificationRepository(SweetManagerContext context) : BaseRepository<TypeNotification>(context), ITypeNotificationRepository
     {
-        private readonly SweetManagerContext _context;
-
-        public TypeNotificationRepository(SweetManagerContext context): base(context)
-        {
-            _context = context;
-        }
+        public async Task<bool> FindByNameAsync(string name)
+            => await Context.Set<TypeNotification>().AnyAsync(t => t.Name.Equals(name));
+        
     }
 }
