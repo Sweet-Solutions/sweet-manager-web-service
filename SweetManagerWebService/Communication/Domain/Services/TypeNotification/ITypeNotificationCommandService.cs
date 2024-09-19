14969a0221563467b55d800b60f0811c04a4e44a
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-19 15:42:51 -0500

    feat: added type notification command service.

---
+++ SweetManagerWebService/Communication/Domain/Services/TypeNotification/ITypeNotificationCommandService.cs
@@ -0,0 +1,9 @@
+﻿using SweetManagerWebService.Communication.Domain.Model.Commands;
+
+namespace SweetManagerWebService.Communication.Domain.Services.TypeNotification;
+
+public interface ITypeNotificationCommandService
+{
+    Task<bool> Handle(SeedTypeNotificationsCommand command);
+    
+}
---
+++ SweetManagerWebService/ResourceManagement/Domain/Services/TypeReport/ITypeReportCommandService.cs
@@ -0,0 +1,9 @@
+﻿using SweetManagerWebService.ResourceManagement.Domain.Model.Commands;
+
+namespace SweetManagerWebService.ResourceManagement.Domain.Services.TypeReport;
+
+public interface ITypeReportCommandService
+{
+    Task<bool> Handle(SeedTypeReportsCommand command);
+    
+}
