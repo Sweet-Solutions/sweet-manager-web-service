a53089a0634e116b279a5363a38e7f8a30fe46eb
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-19 15:51:10 -0500

    feat: added type notification initializer.

---
+++ SweetManagerWebService/Communication/Infrastructure/Population/TypeNotifications/TypeNotificationsInitializer.cs
@@ -0,0 +1,25 @@
+﻿using SweetManagerWebService.Communication.Domain.Model.Commands;
+using SweetManagerWebService.Communication.Domain.Model.Queries.TypeNotification;
+using SweetManagerWebService.Communication.Domain.Services.TypeNotification;
+using SweetManagerWebService.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace SweetManagerWebService.Communication.Infrastructure.Population.TypeNotifications;
+
+public class TypeNotificationsInitializer(ITypeNotificationCommandService typeNotificationCommandService,
+    ITypeNotificationQueryService typeNotificationQueryService, SweetManagerContext context)
+{
+    public async Task InitializeAsync()
+    {
+        // Check if the role table is empty
+
+        var result = await typeNotificationQueryService.Handle(new GetAllTypesNotificationsQuery());
+
+        if (!result.Any())
+        {
+            // Prepopulate the empty table
+            
+            await typeNotificationCommandService.Handle(new SeedTypeNotificationsCommand());
+
+        }
+    }
+}
