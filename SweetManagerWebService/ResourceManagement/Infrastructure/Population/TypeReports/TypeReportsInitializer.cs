c39a1b2824dc53ef93fc5e1e9033cb89c5532af1
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-19 15:51:45 -0500

    feat: added type reports initializer.

---
+++ SweetManagerWebService/ResourceManagement/Infrastructure/Population/TypeReports/TypeReportsInitializer.cs
@@ -0,0 +1,24 @@
+﻿using SweetManagerWebService.ResourceManagement.Domain.Model.Commands;
+using SweetManagerWebService.ResourceManagement.Domain.Model.Queries.TypeReport;
+using SweetManagerWebService.ResourceManagement.Domain.Services.TypeReport;
+using SweetManagerWebService.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace SweetManagerWebService.ResourceManagement.Infrastructure.Population.TypeReports;
+
+public class TypeReportsInitializer(ITypeReportCommandService typeReportCommandService,
+    ITypeReportQueryService typeReportQueryService, SweetManagerContext context)
+{
+    public async Task InitializeAsync()
+    {
+        // Check if the type reports table is empty
+        var result = await typeReportQueryService.Handle(new GetAllTypesReportsQuery());
+
+        if (!result.Any())
+        {
+            // Prepopulate the empty table
+
+            await typeReportCommandService.Handle(new SeedTypeReportsCommand());
+
+        }
+    }
+}
