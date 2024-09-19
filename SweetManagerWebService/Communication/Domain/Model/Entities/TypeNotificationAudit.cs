e51ad437dc202225602ea8c5beaecfa663e35a27
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-19 15:49:17 -0500

    feat: added type notification auditory.

---
+++ SweetManagerWebService/Communication/Domain/Model/Entities/TypeNotificationAudit.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+
+namespace SweetManagerWebService.Communication.Domain.Model.Entities;
+
+public partial class TypeNotificationAudit : IEntityWithCreatedUpdatedDate
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    
+    [Column("UpdateAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
