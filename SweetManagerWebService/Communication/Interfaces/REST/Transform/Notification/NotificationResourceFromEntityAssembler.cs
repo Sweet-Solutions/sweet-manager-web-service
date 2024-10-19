b5cf94dcea2688f992bb9e6559f6b2b9556a326b
Mauricio <mauriciorojasanchez@gmail.com>
2024-10-19 01:05:42 -0500

    fix: updated resource notification.

--- SweetManagerWebService/Communication/Interfaces/REST/Transform/Notification/NotificationResourceFromEntityAssembler.cs
+++ SweetManagerWebService/Communication/Interfaces/REST/Transform/Notification/NotificationResourceFromEntityAssembler.cs
@@ -4,7 +4,7 @@ namespace SweetManagerWebService.Communication.Interfaces.REST.Transform.Notific
 {
     public class NotificationResourceFromEntityAssembler
     {
-        public static CreateNotificationResource ToResourceFromEntity(Domain.Model.Aggregates.Notification entity) =>
-            new(entity.TypesNotificationsId, entity.OwnersId ?? 0, entity.AdminsId ?? 0, entity.WorkersId ?? 0, entity.Title, entity.Description);
+        public static NotificationResource ToResourceFromEntity(Domain.Model.Aggregates.Notification entity) =>
+            new(entity.Id, entity.TypesNotificationsId, entity.OwnersId, entity.AdminsId, entity.WorkersId, entity.Title, entity.Description, entity.CreatedDate);
     }
 }
