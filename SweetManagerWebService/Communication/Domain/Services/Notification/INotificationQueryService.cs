a849fc154e1766d7db74edb3335c72079070e093
Mauricio <mauriciorojasanchez@gmail.com>
2024-10-27 18:37:10 -0500

    feat: added reading for admin and workers notifications in query service.

--- SweetManagerWebService/Communication/Domain/Services/Notification/INotificationQueryService.cs
+++ SweetManagerWebService/Communication/Domain/Services/Notification/INotificationQueryService.cs
@@ -7,4 +7,9 @@ public interface INotificationQueryService
     Task<IEnumerable<Model.Aggregates.Notification>> Handle(GetAllNotificationsQuery query);
 
     Task<Model.Aggregates.Notification?> Handle(GetNotificationByIdQuery query);
+
+    Task<IEnumerable<Model.Aggregates.Notification>> Handle(GetAllNotificationsByWorkerIdQuery query);
+
+    Task<IEnumerable<Model.Aggregates.Notification>> Handle(GetAllNotificationsByHotelIdAndExistOwnersIdQuery query);
+    
 }
