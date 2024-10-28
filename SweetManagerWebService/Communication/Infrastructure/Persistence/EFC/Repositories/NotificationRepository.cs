6955fe1f56a85009fb61de2b931bbd9070a15860
Mauricio <mauriciorojasanchez@gmail.com>
2024-10-27 21:03:04 -0500

    feat: added two readings in notification repository.

--- SweetManagerWebService/Communication/Infrastructure/Persistence/EFC/Repositories/NotificationRepository.cs
+++ SweetManagerWebService/Communication/Infrastructure/Persistence/EFC/Repositories/NotificationRepository.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using SweetManagerWebService.Communication.Domain.Model.Aggregates;
 using SweetManagerWebService.Communication.Domain.Repositories;
 using SweetManagerWebService.IAM.Domain.Model.Aggregates;
+using SweetManagerWebService.IAM.Domain.Model.Entities.Assignments;
 using SweetManagerWebService.Profiles.Domain.Model.Entities;
 using SweetManagerWebService.Shared.Infrastructure.Persistence.EFC.Configuration;
 using SweetManagerWebService.Shared.Infrastructure.Persistence.EFC.Repositories;
@@ -36,6 +37,41 @@ namespace SweetManagerWebService.Communication.Infrastructure.Persistence.EFC.Re
             var result = await queryAsync;
 
             return result;
+        }
+
+        public async Task<IEnumerable<Notification>> FindAllByWorkerIdAsync(int workerId)
+        {
+            Task<IEnumerable<Notification>> queryAsync = new(() => (
+                from wo in Context.Set<Worker>().ToList()
+                join ass in Context.Set<AssignmentWorker>().ToList() on wo.Id equals ass.WorkersId
+                join ad in Context.Set<Admin>().ToList() on ass.AdminsId equals ad.Id
+                join no in Context.Set<Notification>().ToList() on ad.Id equals no.AdminsId
+                where wo.Id.Equals(workerId)
+                select no
+            ));
+            
+            queryAsync.Start();
+
+            var result = await queryAsync;
+
+            return result;
+        }
+
+        public async Task<IEnumerable<Notification>> FindAllByHotelIdAndExitsOwnersIdAsync(int hotelId)
+        {
+            Task<IEnumerable<Notification>> queryAsync = new(() => (
+                from no in Context.Set<Notification>().ToList()
+                join ow in Context.Set<Owner>().ToList() on no.OwnersId equals ow.Id
+                join ho in Context.Set<Hotel>().ToList() on ow.Id equals ho.OwnersId
+                where ho.Id.Equals(hotelId)
+                select no
+            ));
+            
+            queryAsync.Start();
+
+            var result = await queryAsync;
+            
+            return result.Where(n => n is { OwnersId: not null, AdminsId: null, WorkersId: null });
         }
     }
 }
