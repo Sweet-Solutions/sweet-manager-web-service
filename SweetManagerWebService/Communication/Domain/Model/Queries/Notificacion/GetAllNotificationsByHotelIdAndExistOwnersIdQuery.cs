53b00bb66a18fb3b3d07cb2c0131701ea1860c03
Mauricio <mauriciorojasanchez@gmail.com>
2024-10-27 21:03:27 -0500

    feat: added queries for reading context.

---
+++ SweetManagerWebService/Communication/Domain/Model/Queries/Notificacion/GetAllNotificationsByHotelIdAndExistOwnersIdQuery.cs
@@ -0,0 +1,3 @@
+namespace SweetManagerWebService.Communication.Domain.Model.Queries.Notificacion;
+
+public record GetAllNotificationsByHotelIdAndExistOwnersIdQuery(int HotelId);
---
+++ SweetManagerWebService/Communication/Domain/Model/Queries/Notificacion/GetAllNotificationsByWorkerIdQuery.cs
@@ -0,0 +1,3 @@
+namespace SweetManagerWebService.Communication.Domain.Model.Queries.Notificacion;
+
+public record GetAllNotificationsByWorkerIdQuery(int WorkersId);
